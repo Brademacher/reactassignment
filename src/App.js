f501033932ea152b5cfcf3c20f522bf3176cae2c
unknown <benjamin.rademacher@gmail.com>
2018-07-25 18:39:33 -0500

    resolved some of the styling issues, and correctly importing json data

--- src/App.js
+++ src/App.js
@@ -2,33 +2,31 @@ import React, { Component } from "react";
 import PictureCard from "./components/PictureCard";
 import Wrapper from "./components/Wrapper";
 import Title from "./components/Title";
-import friends from "./friends.json";
+import pictures from "./pictures.json";
 import "./App.css";
 
 class App extends Component {
-  // Setting this.state.friends to the friends json array
+  // Setting this.state.pictures to the pictures json array
   state = {
     pictures
   };
 
-  removePicture = id => {
-    // Filter this.state.friends for friends with an id not equal to the id being removed
-    const pictures = this.state.pictures.filter(picture => picture.id !== id);
-    // Set this.state.friends equal to the new friends array
-    this.setState({ pictures });
-  };
+          // removePicture = id => {
+          //   // Filter this.state.pictures for pictures with an id not equal to the id being removed
+          //   const pictures = this.state.pictures.filter(picture => picture.id !== id);
+          //   // Set this.state.pictures equal to the new pictures array
+          //   this.setState({ pictures });
+          // };
 
-  // Map over this.state.friends and render a FriendCard component for each friend object
+  // Map over this.state.pictures and render a PictureCard component for each picture object
   render() {
     return (
       <Wrapper>
         <Title>Pictures</Title>
-        {this.state.friends.map(friend => (
-          <FriendCard
+        {this.state.pictures.map(picture => (
+          <PictureCard
             removePicture={this.removePicture}
             id={picture.id}
-            key={picture.id}
-            name={picture.name}
             image={picture.image}
           />
         ))}
--- src/components/PictureCard/PictureCard.js
+++ src/components/PictureCard/PictureCard.js
@@ -6,16 +6,9 @@ const PictureCard = props => (
     <div className="img-container">
       <img alt={props.name} src={props.image} />
     </div>
-    <div className="content">
-      <ul>
-        <li>
-          <strong>Name:</strong> {props.name}
-        </li>
-      </ul>
-    </div>
-    <span onClick={() => props.removePicture(props.id)} className="remove">
+    {/* <span onClick={() => props.removePicture(props.id)} className="remove">
       𝘅
-    </span>
+    </span> */}
   </div>
 );
 
